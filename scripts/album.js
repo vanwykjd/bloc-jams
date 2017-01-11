c6daaf9d7169960f943b0922a609a7ef2ea6e492
Josh Van Wyk <joshvanwyk@ymail.com>
2017-01-11 22:58:51 +0000

    Checkpoint 33 - jQ Buzz Library: updated/complete

--- scripts/album.js
+++ scripts/album.js
@@ -54,8 +54,8 @@ var createSongRow = function(songNumber, songName, songLength) {
 		    updatePlayerBarSong();
         } else if (currentlyPlayingSongNumber === songNumber) {
             if (currentSoundFile.isPaused()) {
-                $(this).html(playButtonTemplate);
-                $('.main-controls .play-pause').html(playerBarPlayButton);
+                $(this).html(pauseButtonTemplate);
+                $('.main-controls .play-pause').html(playerBarPauseButton);
                 currentSoundFile.play();
             } else {
                 $(this).html(playButtonTemplate);
